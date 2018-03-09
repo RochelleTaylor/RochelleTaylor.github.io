1dd87229f9db4fc519ef4ef567d871227391cc31
Rochelle Taylor <rochelletaylor614@gmail.com>
2018-03-09 16:15:34 -0500

    testing setup

---
+++ spec/js/app.spec.js
@@ -0,0 +1,8 @@
+describe('Portfolio JavaScript Testing',()=>{
+	describe('A method that changes an element\'s visability', () => {
+		it( 'changes an element class from hidden to show',() => {
+
+		})
+
+	})
+})
